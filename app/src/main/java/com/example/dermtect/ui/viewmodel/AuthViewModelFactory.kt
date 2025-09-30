acc54d078e66081dec913a0c066a0095c948c7f3
masuu <johnsonroquejr65@gmail.com>
2025-09-30 10:38:08 +0800

    Update AuthViewModelFactory.kt
    login retain

--- app/src/main/java/com/example/dermtect/ui/viewmodel/AuthViewModelFactory.kt
+++ app/src/main/java/com/example/dermtect/ui/viewmodel/AuthViewModelFactory.kt
@@ -2,13 +2,17 @@ package com.example.dermtect.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.dermtect.data.repository.AuthRepositoryImpl
 import com.example.dermtect.domain.usecase.AuthUseCase
 
-class AuthViewModelFactory : ViewModelProvider.Factory {
+class AuthViewModelFactory(
+    private val authUseCase: AuthUseCase
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val repository = AuthRepositoryImpl()
-        val useCase = AuthUseCase(repository)
-        return AuthViewModel(useCase) as T
+        if (modelClass.isAssignableFrom(AuthViewModel::class.java)) {
+            return AuthViewModel(authUseCase) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
     }
 }
